26e861c80973e9cdba17c9f71b429d8b855f8c57
Marvelous-iAcademy <130442240+Marvelous-iAcademy@users.noreply.github.com>
2023-07-26 17:13:59 +0100

    CQRS

--- RepositoryPattern-Repository/Data/AppDbContext.cs
+++ RepositoryPattern-Repository/Data/AppDbContext.cs
@@ -5,13 +5,21 @@ namespace RepositoryPattern_Repository.Data
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> option) : base(option)
+        {  
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Product> Products { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+      /*  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source=your_database.db");
-        }
+        }*/
     }
 
     
---
+++ RepositoryPattern-Repository/Repository/Abstractions/IGenericRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace RepositoryPattern_Repository.Repository.Abstractions
+{
+    public interface IGenericRepository<T> 
+    {
+        void DeleteRange(IEnumerable<T> entities);
+        void Update(T entity);
+        Task CreateAsync(T entity);
+    }
+}
